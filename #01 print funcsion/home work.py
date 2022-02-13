6dd28d82b14a07fef7c2c7d67f798469ecb6e667
Fayzullo Saydullayev <99532504+fayzulloo@users.noreply.github.com>
2022-02-13 13:52:41 +0500

    Update home work.py

--- #01 print funcsion/home work.py
+++ #01 print funcsion/home work.py
@@ -1,1 +1,22 @@
 
+
+#Saydullayev Fayzullo
+#02/13/2022
+#home work
+
+print("\"Nexia\", \"Tico\", 'Damas' ko'rganlar qilar havas\n")
+
+#5 ning 4-darajasini toping
+print("5ning 4-darajasi", 5 ** 4,'\n') 
+
+#22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+print("22ni 4ga bo'lgandagi qoldiq", 22 % 4, '\n') 
+
+#Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+print("Yuzi S =", 125 * 125, "Perimetri P =", 125 * 4,'\n')
+
+#Diametri 12 ga teng bo'lgan doiraning yuzini toping (π = 3.14 deb oling)
+print("Doira yuzi S =", 3.14 * (12/2) ** 2,'\n')
+
+#Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+print("a = 6, b = 7, c = ?\n", "c =", (6 ** 2 + 7 ** 2) ** 0.5)
