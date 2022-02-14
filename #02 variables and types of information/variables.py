bf07de46be6cc0464357a221833ad6e650608259
Fayzullo Saydullayev <99532504+fayzulloo@users.noreply.github.com>
2022-02-14 14:55:06 +0500

    Update and rename main.py to variables.py

---
+++ #02 variables and types of information/variables.py
@@ -0,0 +1,18 @@
+
+#Saydullayev Fayzullo
+#02/14/2022
+#work with variables
+#home work
+
+text = "Hello World!"
+print(text)
+
+message = "Hi i'm Fayzullo"
+print(message)
+message = "I'm 21 years old"
+print(message)
+
+a = 5
+b = 7
+c = (a + b) ** 2
+print(c)
